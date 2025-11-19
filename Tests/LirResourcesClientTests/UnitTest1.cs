8c19fecae958946a66f4ef109520bb9ba7b314b9
Dmitry Scherbakov <git@sherbacov.com>
2025-11-19 19:08:58 +0300

    Refactor LirResourcesClientTests to improve null/whitespace API key handling and remove redundant `GetApiKey` method.

--- Tests/LirResourcesClientTests/UnitTest1.cs
+++ Tests/LirResourcesClientTests/UnitTest1.cs
@@ -13,28 +13,23 @@ public class LirResourcesClientIntegrationUnitTests
     {
         _apiKey = Environment.GetEnvironmentVariable("LIR_RESOURCES_API_KEY");
 
-        if (_apiKey != null)
+        if (!string.IsNullOrWhiteSpace(_apiKey))
         {
             _client = new LirResourcesClient(new LirResourcesProductionLocation());
             _client.Debug = true;
         }
     }
 
-    private string GetApiKey()
-    {
-        return _apiKey;
-    }
-    
     [Fact]
     public async Task RequestIpv4()
     {
-        if (_apiKey == null || _client == null)
+        if (string.IsNullOrWhiteSpace(_apiKey) || _client == null)
         {
             // Skip test if API key is not provided
             return;
         }
 
-        var ipv4 = await _client.GetIpv4(GetApiKey()!);
+        var ipv4 = await _client.GetIpv4(_apiKey);
         Assert.True(ipv4.Ipv4Allocations.Any(), "No Ipv4 PA networks from LIR API.");
         Assert.True(ipv4.Ipv4Assignments.Any(), "No Ipv4 PI networks from LIR API.");
     }
@@ -42,26 +37,26 @@ public class LirResourcesClientIntegrationUnitTests
     [Fact]
     public async Task RequestIpv6()
     {
-        if (_apiKey == null || _client == null)
+        if (string.IsNullOrWhiteSpace(_apiKey) || _client == null)
         {
             // Skip test if API key is not provided
             return;
         }
 
-        var ipv6 = await _client.GetIpv6(GetApiKey()!);
+        var ipv6 = await _client.GetIpv6(_apiKey);
         Assert.True(ipv6.Ipv6Allocations.Any(), "No Ipv6 networks from LIR API.");
     }
     
     [Fact]
     public async Task RequestAll()
     {
-        if (_apiKey == null || _client == null)
+        if (string.IsNullOrWhiteSpace(_apiKey) || _client == null)
         {
             // Skip test if API key is not provided
             return;
         }
 
-        var resources = await _client.GetResources(GetApiKey()!);
+        var resources = await _client.GetResources(_apiKey);
 
         Assert.True(resources.Ipv4Allocations.Any(), "No Ipv4 PA networks from LIR API.");
         Assert.True(resources.Ipv4Assignments.Any(), "No Ipv4 PI networks from LIR API.");
